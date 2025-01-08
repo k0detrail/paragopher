21862c1de027f1bc406124d8b8c2407bc1152f17
Yegor Stepanov <ystepanoff@icloud.com>
2025-01-08 21:45:16 +0000

    Add basic config: screen dimensions & colours

---
+++ internal/config/config.go
@@ -0,0 +1,21 @@
+package config
+
+import "image/color"
+
+const (
+	ScreenWidth  = 800
+	ScreenHeight = 600
+
+	BarrelAngleMin = -90.0
+	BarrelAngleMax = 90.0
+)
+
+var (
+	BaseW = ScreenWidth / 10
+	BaseH = ScreenHeight / 10
+
+	ColourTeal  = color.RGBA{R: 101, G: 247, B: 246, A: 255}
+	ColourPink  = color.RGBA{R: 255, G: 82, B: 242, A: 255}
+	ColourWhite = color.RGBA{R: 255, G: 255, B: 255, A: 255}
+	ColourBlack = color.RGBA{R: 0, G: 0, B: 0, A: 255}
+)
