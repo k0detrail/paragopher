67d94a3eea6d75d05cfd54417a9387f87e62abc3
Yegor Stepanov <ystepanoff@icloud.com>
2025-01-08 22:01:15 +0000

    Define basic game state

--- cmd/game.go
+++ cmd/game.go
@@ -1,4 +1,14 @@
 package main
 
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/ystepanoff/paragophers/internal/config"
+	"github.com/ystepanoff/paragophers/internal/config/game"
+)
+
 func main() {
+	ebiten.SetWindowSize(config.ScreenWidth, config.ScreenHeight)
+	ebiten.SetWindowTitle("ParaGophers")
+
+	g := &game.Game{}
 }
